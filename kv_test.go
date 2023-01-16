f450ed2a9f4a5c3d3671ab9a8e942078c6fcf8ef
bashery <ahmedalbachiry@gmail.com>
2023-01-16 16:28:14 +0300

    improve unit testing

--- kv_test.go
+++ kv_test.go
@@ -1,26 +1,67 @@
 package kv
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 var m = New[int, int]()
 
-func Test_Set(t *testing.T) {
-	t.Parallel()
-	for i := 0; i < 1000000; i++ {
+// testing Set Get HasKey & delet functions
+func Test_runAll(t *testing.T) {
+	fmt.Println("================= sync mod ===================")
 
-		m.Set(i, i*2)
+	intmap := New[int, int]()
+	intmap.Set(1, 123)
+	intval, _ := intmap.Get(1)
+	if intval != 123 {
+		t.Errorf("key must be %d", 123)
 	}
+
+	if ok := intmap.HasKey(1); ok != true {
+		t.Error("'1' key must be exist")
+	}
+
+	//
+	strmap := New[string, string]()
+	strmap.Set("hi", "hello")
+	sval, _ := strmap.Get("hi")
+	if sval != "hello" {
+		t.Errorf("value must be %s", "hello")
+	}
+
+	if ok := strmap.HasKey("hi"); ok != true {
+		t.Error("'hi' key must be exist")
+	}
+
+	fmt.Println("All Functions pass")
+	fmt.Println("=============== concurrent mod ==============")
 }
 
+// testing Set and HasKey function in parallel
+func Test_Set_HasKey(t *testing.T) {
+	t.Parallel()
+	for i := 0; i < 1000000; i++ {
+		if ok := m.HasKey(i); ok == true {
+			t.Errorf("%d key must be not exist", i)
+		}
+		m.Set(i, i*2)
+		if ok := m.HasKey(i); ok == false {
+			t.Errorf("%d key must be exist", i)
+		}
+	}
+	fmt.Println("Set func Pass")
+	fmt.Println("HasKey func Pass")
+}
+
+// testing Get function in parallel
 func Test_Get(t *testing.T) {
 	t.Parallel()
 	time.Sleep(time.Millisecond * 100)
 
 	for i := 0; i < 1000000; i++ {
-
+		time.Sleep(time.Nanosecond * 1)
 		val, err := m.Get(i)
 		if err != nil {
 			t.Error(err)
@@ -33,33 +74,23 @@ func Test_Get(t *testing.T) {
 
 	}
 
+	fmt.Println("Get func Pass ")
 }
 
-func Test_HasKey(t *testing.T) {
+// testing Delet function in parallel
+func Test_Delete(t *testing.T) {
 	t.Parallel()
 	time.Sleep(time.Millisecond * 150)
 
 	for i := 0; i < 1000000; i++ {
-		yes := m.HasKey(i)
-		if yes != true {
-			t.Errorf("%d must be exist", i)
-			break
-			// return
-		}
-	}
-}
-
-func Test_Delete(t *testing.T) {
-	t.Parallel()
-	time.Sleep(time.Millisecond * 200)
-
-	for i := 0; i < 1000000; i++ {
+		time.Sleep(time.Nanosecond * 1)
 		m.Delete(i)
 	}
 	if len(m.data) != 0 {
 		t.Errorf("len data must be %d not %d\n", 0, len(m.data))
 	}
 
+	fmt.Println("Delete functions Pass")
 }
 
 /*
